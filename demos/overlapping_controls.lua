4acaa5473e8617bae532689433495deef371f2c3
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-08-31 22:29:15 +0200

    add overlapping control demo

---
+++ demos/overlapping_controls.lua
@@ -0,0 +1,45 @@
+local path_root = debug.getinfo(1).source:sub(2):gsub('\\[^\\]+\\[^\\]+$', '\\')
+
+---@module "breitbandgraphics"
+BreitbandGraphics = dofile(path_root .. 'breitbandgraphics.lua')
+
+---@module "mupen-lua-ugui"
+ugui = dofile(path_root .. 'mupen-lua-ugui.lua')
+
+---@module "mupen-lua-ugui-ext"
+ugui_ext = dofile(path_root .. 'mupen-lua-ugui-ext.lua')
+
+emu.atdrawd2d(function()
+    local window_size = wgui.info()
+    BreitbandGraphics.fill_rectangle({
+        x = 0,
+        y = 0,
+        width = window_size.width,
+        height = window_size.height,
+    }, {
+        r = 253,
+        g = 253,
+        b = 253,
+    })
+    local keys = input.get()
+    ugui.begin_frame({
+        mouse_position = {
+            x = keys.xmouse,
+            y = keys.ymouse,
+        },
+        wheel = 0,
+        is_primary_down = keys.leftclick,
+        held_keys = keys,
+    })
+    ugui.button({
+        uid = 1,
+        rectangle = {x = 10, y = 10, width = 100, height = 100},
+        text = 'Hello, world!',
+    })
+    ugui.button({
+        uid = 2,
+        rectangle = {x = 80, y = 80, width = 100, height = 50},
+        text = 'Hello, world!',
+    })
+    ugui.end_frame()
+end)
