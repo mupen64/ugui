6786388c12a9e16db373d084401de6130ccccf79
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-03-07 21:13:26 +0100

    refactor: move helpers to separate file (#55)

--- build.py
+++ build.py
@@ -32,6 +32,7 @@ JOBS: list[tuple[list[Path], Path]] = [
             Path("src/ugui/types.lua"),
             Path("src/ugui/core.lua"),
             Path("src/ugui/internal.lua"),
+            Path("src/ugui/helpers.lua"),
             Path("src/ugui/styler.lua"),
             Path("src/ugui/controls/button.lua"),
             Path("src/ugui/controls/toggle_button.lua"),
