56ef9eebc335d80173bf0208090620bcd3c92685
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2026-03-15 11:16:38 +0100

    default STATIC_ENV

--- src/ugui/environment/static_env.lua
+++ src/ugui/environment/static_env.lua
@@ -7,3 +7,10 @@
 ---@class UguiStaticEnvironment
 ---@field clipboard UguiClipboard? The clipboard access provider.
 ---The script's environment that provides access to external facilities.
+
+ugui.STATIC_ENV = {
+    clipboard = {
+        get = function() end,
+        set = function(text) end,
+    },
+}
